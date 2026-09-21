75ee896b40a28b1387512cc3af09546584e94f4a
Pejman <ds2020p@hotmail.com>
2026-09-21 14:08:49 +0330

    Add DoubleToGridLengthConverter for TOTP progress bar width

---
+++ src/PejPass.Wpf/Converters/DoubleToGridLengthConverter.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PejPass.Wpf.Converters;
+
+/// <summary>
+/// Converts 0..1 progress + parent ActualWidth (via MultiBinding) is simpler;
+/// this one converts remaining fraction * maxWidth passed as ConverterParameter,
+/// or just returns a star-less fixed approach via code-behind-free binding to TotpProgressWidth.
+/// Prefer binding TotpProgressWidth (pixels) from ViewModel instead.
+/// </summary>
+public sealed class PassThroughDoubleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value ?? 0.0;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
