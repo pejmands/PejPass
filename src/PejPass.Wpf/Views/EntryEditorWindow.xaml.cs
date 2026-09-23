77d2e04e742f96ec5e719488267220eedb863ebe
Pejman <ds2020p@hotmail.com>
2026-09-24 02:20:02 +0330

    EntryEditor Notes: attach NestedScrollChain without requiring x:Name

--- src/PejPass.Wpf/Views/EntryEditorWindow.xaml.cs
+++ src/PejPass.Wpf/Views/EntryEditorWindow.xaml.cs
@@ -3,6 +3,8 @@ using PejPass.Wpf.Dialogs;
 using PejPass.Wpf.Services;
 using PejPass.Wpf.ViewModels;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace PejPass.Wpf.Views;
 
@@ -17,7 +19,39 @@ public partial class EntryEditorWindow : Window
         Title = viewModel.Original is null ? "Add Entry" : "Edit Entry";
 
         // Nested scroll chain: Notes first, then the form ScrollViewer at boundaries
-        Loaded += (_, _) => NestedScrollChain.Attach(NotesBox);
+        Loaded += (_, _) => AttachNotesScrollChain();
+    }
+
+    private void AttachNotesScrollChain()
+    {
+        foreach (var tb in FindVisualChildren<TextBox>(this))
+        {
+            // Multiline notes box (Height/MaxHeight constrained)
+            if (!tb.AcceptsReturn)
+                continue;
+            if (tb.VerticalScrollBarVisibility == ScrollBarVisibility.Disabled)
+                continue;
+
+            NestedScrollChain.Attach(tb);
+            return;
+        }
+    }
+
+    private static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
+    {
+        if (parent is null)
+            yield break;
+
+        var count = VisualTreeHelper.GetChildrenCount(parent);
+        for (var i = 0; i < count; i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            if (child is T match)
+                yield return match;
+
+            foreach (var nested in FindVisualChildren<T>(child))
+                yield return nested;
+        }
     }
 
     private void Save_Click(object sender, RoutedEventArgs e)
