be09240afe882a8ef31ef5d86ca81fc461524e43
Tondar <tondar20@outlook.com>
2026-09-25 02:43:31 +0330

    Test AES-GCM AAD authentication

---
+++ tests/PejPass.Infrastructure.Tests/Crypto/CryptoServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using PejPass.Infrastructure.Crypto;
+using System.Security.Cryptography;
+
+namespace PejPass.Infrastructure.Tests.Crypto;
+
+public sealed class CryptoServiceTests
+{
+    [Fact]
+    public void Decrypt_WhenAssociatedDataIsModified_ThrowsAuthenticationTagMismatchException()
+    {
+        var crypto = new CryptoService();
+
+        var plaintext = "PejPass test"u8.ToArray();
+        var key = crypto.DeriveKey(
+            "password",
+            crypto.GenerateSalt());
+
+        var associatedData = "PEJP-v1-metadata"u8.ToArray();
+
+        try
+        {
+            var (ciphertext, nonce, tag) = crypto.Encrypt(
+                plaintext,
+                key,
+                associatedData);
+
+            associatedData[0] ^= 0x01;
+
+            Assert.Throws<AuthenticationTagMismatchException>(() =>
+                crypto.Decrypt(
+                    ciphertext,
+                    nonce,
+                    tag,
+                    key,
+                    associatedData));
+        }
+        finally
+        {
+            crypto.ZeroMemory(key);
+        }
+    }
+}
