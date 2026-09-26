a3bc9392681eecf03eb4402291bdb584a51e5f16
Pejman <ds2020p@hotmail.com>
2026-09-26 19:54:34 +0330

    Fix snackbar popup imports

--- src/PejPass.Wpf/MainWindow.xaml.cs
+++ src/PejPass.Wpf/MainWindow.xaml.cs
@@ -4,8 +4,10 @@ using PejPass.Wpf.ViewModels;
 using PejPass.Wpf.Views;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace PejPass.Wpf;
 
